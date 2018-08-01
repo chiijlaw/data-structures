98cec9ffd4b42e1b3ac1812418178f9cd990f889
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-01 16:00:06 -0700

    Queue for shared patter passes all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.count = 0;
+  extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var queueMethods = {
+  enqueue: function(value) {
+    this[value] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    this.count--;
+    var mostRecentItem = Object.keys(this)[4];
+    output = mostRecentItem;
+    if (Object.keys(this).length > 4) {
+      delete this[mostRecentItem];
+      return output;
+    }
+  },
+  size: function() {
+    if (this.count <= 0) {
+      return 0;
+    } else {
+      return this.count;
+    }
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -12,32 +12,32 @@ var Stack = function() {
 };
 
 var extend = function (to, from) {
-	for (var key in from) {
-		to[key] = from[key];
-	}
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {
-	push: function (val) {
-    	this[val] = val;
-		this.count++;
-	},
-	pop: function () {
-		this.count--;
-		var lastItem = Object.keys(this)[Object.keys(this).length - 1];
-		output = lastItem;
-		if (Object.keys(this).length > 4) {
-			delete this[lastItem];
-			return output;
-		}
-	},
-	size: function () {
-		if (this.count <= 0) {
-      		return 0;
-    	} else {
-      		return this.count;
-    	}
+  push: function (val) {
+    this[val] = val;
+    this.count++;
+  },
+  pop: function () {
+    this.count--;
+    var lastItem = Object.keys(this)[Object.keys(this).length - 1];
+    output = lastItem;
+    if (Object.keys(this).length > 4) {
+      delete this[lastItem];
+      return output;
     }
+  },
+  size: function () {
+    if (this.count <= 0) {
+      return 0;
+    } else {
+      return this.count;
+    }
+  }
 };
 
 
