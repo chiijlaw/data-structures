3e27aa2cfe9d844e893a5742338d6d6e6cb4856e
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-01 16:20:12 -0700

    Stack for prototypal patter passes all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,9 +2,6 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var newStack = {};
-  // newStack.push = push;
-  // newStack.pop = pop;
-  // newStack.size = size;
   newStack.count = 0;
   extend(newStack, stackMethods);
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.count = 0;
+  newStack.output = 0;
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (val) {
+  this[val] = val;
+  this.count++;
+};
+
+stackMethods.pop = function () {
+  this.count--;
+  var lastItem = Object.keys(this)[Object.keys(this).length - 1];
+  output = lastItem;
+  if (Object.keys(this).length > 2) {
+    delete this[lastItem];
+    return output;
+  }
+};
+
+stackMethods.size = function () {
+  if (this.count <= 0) {
+    return 0;
+  } else {
+    return this.count;
+  }
+};
 
