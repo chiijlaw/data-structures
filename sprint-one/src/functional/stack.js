6495c93c3c528a5bdae71241654221b67a30b4bf
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-01 12:33:02 -0700

    Passes all but the pop tests for stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  var output = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    var value = value;
+    someInstance[value] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    count--;
+    var lastItem = Object.keys(someInstance)[Object.keys(someInstance).length-1];
+    output = lastItem;
+    if (Object.keys(someInstance).length-1 > 3) {
+      delete someInstance[lastItem];
+      return output;
+    }
   };
 
   someInstance.size = function() {
+    if (count <= 0) {
+      return 0;
+    } else {
+      return count;
+    }
   };
 
   return someInstance;
