5ccc96bcc4920bca0e971c959fc28e1c7aa034e4
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-01 17:34:16 -0700

    Stack for ES6 pattern passes all tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,31 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+  	this.output = 0;
+  }
+
+  push(val) {
+    this[val] = val;
+    this.count++;
+  }
+
+  pop() {
+    this.count--;
+    var lastItem = Object.keys(this)[Object.keys(this).length - 1];
+    this.output = lastItem;
+    if (Object.keys(this).length > 2) {
+      delete this[lastItem];
+      return this.output;
+    }
+  }
+
+  size() {
+    if (this.count <= 0) {
+      return 0;
+    } else {
+      return this.count;
+    }
   }
 
 }
