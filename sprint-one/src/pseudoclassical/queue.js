c900048727e0548f345bfba77ae8c34a75614084
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-01 17:26:16 -0700

    The last part of part1 for sprint1 finishes. Queue for pseudoclassical pattern passes all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.output = 0;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this[value] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function () {
+  this.count--;
+  var mostRecentItem = Object.keys(this)[2];
+  output = mostRecentItem;
+  if (Object.keys(this).length > 2) {
+    delete this[mostRecentItem];
+    return output;
+  }
+};
+
+Queue.prototype.size = function ()	{
+  if (this.count <= 0) {
+    return 0;
+  } else {
+    return this.count;
+  }
+};
