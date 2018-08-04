cc488462d0cf9706c08ac4c94811b1a7c3e75fe2
chiijlaw <chiithird@gmail.com>
2018-08-03 18:44:21 -0700

    binarySearchTree passes all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,24 +1,61 @@
 var BinarySearchTree = function(value) {
   var biTree = {};
-  biTree.left;
-  biTree.right;
+  biTree.value = value;
+  biTree.left = null;
+  biTree.right = null;
   _.extend(biTree, biTreeMethods);
   return biTree;
 };
 
 var biTreeMethods = {};
 
-biTreeMethods.insert = function() {
-
+biTreeMethods.insert = function(value) {
+  if (this.value < value) {
+    if (this.right !== null) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  } else {
+    if (this.left !== null) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  }
 };
 
-biTreeMethods.contains = function() {
-
+biTreeMethods.contains = function(target) {
+  var result = false;  
+  if (this.value === target) {
+    return result = true;
+  } else if (this.value < target) {
+    if (this.right === null) {
+      return false;
+    }
+    return result = this.right.contains(target);  
+  } else if (this.value > target) {
+    if (this.left === null) {
+      return false;
+    }
+    return result = this.left.contains(target);
+  }
+  
+  return result;
 };
 
-biTreeMethods.depthFirstLog = function() {
-
+biTreeMethods.depthFirstLog = function(fun) {
+  fun(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(fun);
+  } 
+  if ( this.right !== null) {
+    this.right.depthFirstLog(fun);
+  }
 };
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert is logerithmic O(log)
+  contains is logerithmic O(log)
+  depthFirstLog linear O(n)
  */
