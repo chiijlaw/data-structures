6d8e1d4ec1911f304bdad2e14e22123a913c7a28
chiijlaw <chiithird@gmail.com>
2018-08-03 17:34:05 -0700

    hasTable passes all tests. BinarySearchTree contains methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,24 @@
 var BinarySearchTree = function(value) {
+  var biTree = {};
+  biTree.left;
+  biTree.right;
+  _.extend(biTree, biTreeMethods);
+  return biTree;
 };
 
+var biTreeMethods = {};
 
+biTreeMethods.insert = function() {
+
+};
+
+biTreeMethods.contains = function() {
+
+};
+
+biTreeMethods.depthFirstLog = function() {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,49 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value = [k,v];
+  if (Array.isArray(this._storage[index])) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+        break;
+      }
+    }  
+    this._storage[index].push(value);
+  } else {
+    this._storage[index] = [];
+    this._storage[index].push(value);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      delete this._storage[index][i][1];
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert is linear O(n)
+  retrieve is constant O(1)
+  remove is linear O(n)
+  
  */
 
 
